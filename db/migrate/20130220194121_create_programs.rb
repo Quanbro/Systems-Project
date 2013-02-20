cf31ddf3a0459187ce7f753361058267a6de0156
ScottMontgomery <scott@scottmontgomery.ca>
2013-02-20 15:01:20 -0500

    generated role model

--- db/migrate/20130220194121_create_programs.rb
+++ db/migrate/20130220194121_create_programs.rb
@@ -1,7 +1,7 @@
 class CreatePrograms < ActiveRecord::Migration
   def change
     create_table :programs do |t|
-    	t.string  "NAME",             :null => false	
+    	t.string  "name",             :null => false	
       t.timestamps
     end
   end
