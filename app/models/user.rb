5ae46f08eef21be6efd2f052e22f214480477696
ScottMontgomery <scott@scottmontgomery.ca>
2013-03-05 10:51:30 -0500

    Readded sqlite3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	acts_as_authentic
+  acts_as_authentic
   attr_accessible  :email, :password, :password_confirmation, :perishable_token, :created_at, :role, :first_name, :last_name
   validates :first_name, :presence => {:message => 'cannot be blank'}
   validates :last_name, :presence => {:message => 'cannot be blank'}
