e3a09d48b108dd27e6f86fd9740879d4526661f5
ScottMontgomery <scott@scottmontgomery.ca>
2013-03-12 20:01:00 -0400

    added password validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
-  attr_accessible  :email, :password, :password_confirmation, :perishable_token, :created_at, :role, :first_name, :last_name
-  validates :first_name, :presence => {:message => 'cannot be blank'}
-  validates :last_name, :presence => {:message => 'cannot be blank'}
 
+  acts_as_authentic do |c|
+    c.merge_validates_length_of_password_field_options :within => 8..24
+  end # the configuration block is optional  
+
+  attr_accessible  :email, :password, :password_confirmation, :perishable_token, :created_at, :role, :first_name, :last_name
+  validates :password, :format => {:with => /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/, :message => 'must have at least one number and one letter in it'}
+  validates :first_name, :presence => {:message => 'cannot be blank'}
+  validates :last_name, :presence => {:message => 'cannot be blank'} 
 end
