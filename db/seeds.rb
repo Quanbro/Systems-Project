91531e31db48e10fd7b9375ca8fbd4c66d21d699
ScottMontgomery <scott@scottmontgomery.ca>
2013-03-12 20:48:43 -0400

    Created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,26 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+programs = Program.create([{ name: 'Cut With a Knife' }, { name: 'Count Out Objects From Set' }, { name: 'Cut With Scissors' }])
+
+
+admin = User.create(
+    :first_name => "Super",
+    :last_name => "Admin",
+    :email => "admin@gmail.com",
+    :password => "12345678a",
+    :password_confirmation => "12345678a",
+    :login_count => 0,
+    :role => "Admin"
+)
+
+admin = User.create!(
+    :first_name => "Dylan",
+    :last_name => "Scott",
+    :email => "dylan@gmail.com",
+    :password => "12345678a",
+    :password_confirmation => "12345678a",
+    :login_count => 0
+)
