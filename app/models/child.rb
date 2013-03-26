e71f0a29330acf7f9e3a249bc5463058b9153440
ScottMontgomery <scott@scottmontgomery.ca>
2013-03-25 20:40:18 -0400

    updated child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,6 @@
 class Child < ActiveRecord::Base
   attr_accessible :first_name, :last_name
   has_many :programs
+  validates :first_name, :presence => {:message => 'cannot be blank'}
+  validates :last_name, :presence => {:message => 'cannot be blank'}   
 end
